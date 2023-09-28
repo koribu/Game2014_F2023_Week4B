28d71d71b5100b2f6d5868074cf1258aa6fb4540
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-09-28 14:19:21 -0400

    Score system functionality added

--- Assets/[Script]/GameController.cs
+++ Assets/[Script]/GameController.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField]
+    int _score = 0;
+    int _previousScore = 0;
+
+    [SerializeField]
+    TextMeshProUGUI _scoreText;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +22,23 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_score != _previousScore)
+            UpdateScoreUI();
     }
+
+    public void ChangeScore(int scoreChangingAmount)
+    {
+        _score += scoreChangingAmount;
+
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        _scoreText.text = "Score: " + _score;
+
+    }
+
 
    public void LoadGameScene()
     {
